9ac7dea1d6eb8648d1c9264fc0604f96b007bb1d
Renze Nicolai <renze@rnplus.nl>
2025-05-05 00:04:35 +0200

    Is it broken?

--- .validator/validate.js
+++ .validator/validate.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 const Ajv = require("ajv");
 const ajv = new Ajv();
 
+console.log("YES IT IS")
+process.exit(1)
+
 const schema = JSON.parse(fs.readFileSync("schema.json"));
 const data = JSON.parse(fs.readFileSync(process.argv[2]));
 const valid = ajv.validate(schema, data);
